fd10f41568508daaebfac53e6b00245cdc8ff021
KD <gkai70264@gmail.com>
2022-04-28 12:07:53 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Thread-Based Modmailing service
+
+This is a discord bot that works on thread channels to provide good-looking and easy to use modmail-like features.
+
+> *THIS IS UDIM 2!!*
+~ *Some GitHub user in 3043*
---
+++ mod.py
@@ -0,0 +1,54 @@
+import discord
+
+client = discord.Bot(debug_guilds=[969083846110879744], intents=discord.Intents.all())
+
+mod_chan = 969266297504428092
+
+users = {}
+usersX = {}
+usersO = {}
+
+cu = 0;
+
+@client.event
+async def on_message(msg: discord.Message):
+    global cu
+    if (msg.author.bot): return
+    if (cu == None): cu = 0;
+
+    if (msg.guild == None and users.get(msg.author.id) == None):
+        ms = client.get_channel(mod_chan)
+        
+        s = await ms.send("A new thread has opened!")
+        cu += 1
+        eth = await s.create_thread(name="User-" + str(cu), auto_archive_duration=60)
+        users[msg.author.id] = eth.id
+        usersX[eth.id] = msg.author.id
+        usersO[eth.id] = msg.author
+    
+    elif (users.get(msg.author.id) != None and msg.guild == None):
+        await client.get_channel(users[msg.author.id]).send(f"**{msg.author.display_name}:**  " + msg.content)
+
+    elif (usersX.get(msg.channel.id) != None):
+        l = usersO.get(msg.channel.id)
+
+        await l.send(f"**{msg.author.display_name}:** " + msg.content)
+    
+        
+
+
+@client.slash_command(name = "close", description = "Closes the current thread based on the channel you're in.")
+async def _h(ctx):
+    await ctx.respond("closing thread...")
+    
+    
+
+    if (usersO.get(ctx.channel.id) != None):
+        await usersO[ctx.channel.id].send("This thread has been closed by a moderator.")
+        del usersO[ctx.channel.id]
+        del users[ctx.author.id]
+        del usersX[ctx.channel.id]
+        await ctx.channel.delete();
+
+
+client.run("")
